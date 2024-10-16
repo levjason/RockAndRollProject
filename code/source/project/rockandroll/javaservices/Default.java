615064b9b7a5c39c212e11b8a3e50a24318659f7
ldsgreenadmin <ldsgreenadmin>
2024-10-16 17:09:12 +0000

    Commit made by: ldsgreenadmin

--- code/source/project/rockandroll/javaservices/Default.java
+++ code/source/project/rockandroll/javaservices/Default.java
@@ -7,6 +7,8 @@ import com.wm.util.Values;
 import com.wm.app.b2b.server.Service;
 import com.wm.app.b2b.server.ServiceException;
 // --- <<IS-START-IMPORTS>> ---
+import com.softwareag.util.IDataMap;
+import java.lang.String;
 // --- <<IS-END-IMPORTS>> ---
 
 public final class Default
@@ -30,6 +32,30 @@ public final class Default
 	{
 		// --- <<IS-START(MakeItUpper)>> ---
 		// @sigtype java 3.5
+		// [i] field:0:required Str1
+		// [o] field:0:required Str2
+		// --- <<IS-BEGIN-PIPELINE-IN>> ---
+		// WARNING: Auto generate code will not be preserved upon Java signature update.
+		// Do not add custom code here.
+		
+		IDataMap pipelineInMap = new IDataMap(pipeline);
+		String inputStr1 = (String) pipelineInMap.get("Str1");
+		// --- <<IS-END-PIPELINE-IN>> ---
+		
+		// --- <<IS-BEGIN-INSTANCES-PIPELINE-OUT>> ---
+		// WARNING: Auto generate code will not be preserved upon Java signature update.
+		// Do not add custom code here.
+		
+		String outputStr2 = null;
+		// --- <<IS-END-INSTANCES-PIPELINE-OUT>> ---
+		
+		// --- <<IS-BEGIN-PIPELINE-OUT>> ---
+		// WARNING: Auto generate code will not be preserved upon Java signature update.
+		// Do not add custom code here.
+		
+		IDataMap pipelineOutMap = new IDataMap(pipeline);
+		pipelineOutMap.put("Str2", outputStr2);
+		// --- <<IS-END-PIPELINE-OUT>> ---
 		// --- <<IS-END>> ---
 
                 
